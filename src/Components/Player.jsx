f55071688a681b9a6ff3ca6780d2f52cef4a8dab
HolaSoyGonzalo <daved.pacco@gmail.com>
2020-11-29 15:54:50 +0100

    Installed and imported  FA Components

--- src/Components/Player.jsx
+++ src/Components/Player.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Player = () => {
   return (
     <div className="player">
-      <h1>Player</h1>
+      <div className="time-controls">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-controls">
+        <FontAwesomeIcon className="back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon className="play" size="2x" icon={faPlay} />
+        <FontAwesomeIcon className="forward" size="2x" icon={faAngleRight} />
+      </div>
     </div>
   );
 };
